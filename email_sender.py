5f3169b121852df528dbc746091d3a466a22b3d7
raziel-star <rnevo2016@gmail.com>
2025-10-07 20:55:08 +0300

    Update email_sender.py

--- email_sender.py
+++ email_sender.py
@@ -2,15 +2,20 @@ import smtplib
 from email.message import EmailMessage
 
 def send_email(to_email, subject, body, smtp_server, smtp_port, username, password):
+    """
+    SMTP protocol
+    """
     try:
         msg = EmailMessage()
         msg['Subject'] = subject
         msg['From'] = username
         msg['To'] = to_email
         msg.set_content(body)
+        
+        # שימוש ב-SMTP_SSL לחיבור מאובטח (עבור פורט 465)
         with smtplib.SMTP_SSL(smtp_server, smtp_port) as server:
             server.login(username, password)
             server.send_message(msg)
-        return f"Email sent successfully to {to_email}."
+        return f"Email sent successfully to {to_email} via SMTP."
     except Exception as e:
-        return f"Failed to send email: {e}"
+        return f"Failed to send email via SMTP: {e}"
