506ffcf5fceb92978543d29e51f44d050f142e38
raziel-star <rnevo2016@gmail.com>
2025-10-07 20:55:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,15 @@
+# config.py
 
 GOOGLE_API_KEY = "your api key"
 SERPAPI_KEY = "your api key"
 MEMORY_FILE = "cli_chat_memory.pkl"
+
+# --- email sender ---
+# private server.
+SMTP_SERVER = 'smtp.gmail.com' # put your real email! 
+SMTP_PORT = 400 # put your real SMTP_PORT!               
+
+# private entery.
+# private params.
+SENDER_USERNAME = 'your_email_here' 
+SENDER_PASSWORD = 'your_password_here'
